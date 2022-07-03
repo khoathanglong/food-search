f3295a88579eacc9968241eb1ab0ad717f935110
Long Khoa <khoathanglong@gmail.com>
2022-07-03 10:45:48 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import FoodSearch from "./components/FoodSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <FoodSearch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FoodSearch.js
@@ -0,0 +1,5 @@
+const FoodSearch = () => {
+  return <>hello</>;
+};
+
+export default FoodSearch;
